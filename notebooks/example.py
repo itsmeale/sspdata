d14f3775a0fa05bbd563dc97ffbb86c4391c2ced
Alexandre Farias <0800alefarias@gmail.com>
2021-07-01 00:39:30 -0300

    Adds examples on how to use

---
+++ notebooks/example.py
@@ -0,0 +1,19 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.11.3
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+from sspdata.datasets.feminicidio import extrair_feminicidios
+
+df = extrair_feminicidios(ano=2021, mes=3)
+df.head(5)
